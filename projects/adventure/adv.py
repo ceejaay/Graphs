1cad8320bcd4237c6950e88fe5c727c317e3842a
Chad Jemmett <chad.jemmett@gmail.com>
2019-03-15 15:54:40 -0600

    Thought it was working. But I think there are some logic flaws in the BFS.

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -71,41 +71,71 @@ class Mapping:
 
 
     def breadth_first_search(self, start):
+        # print('visited 0', '?' in self.visited[0].values())
         # path only has the id of the room from the dft
-        path = [start]
+        # path = [start]
         searching = True
         cur_room = player.currentRoom
         cur_room_exits = cur_room.getExits()
         prev_room = None
-        while searching:
-            # print('searching')
-            # print('traversal', traversalPath)
-        # for i in range(3):
-            # print('cur room', player.currentRoom.id)
-            # print('cur room exits', cur_room_exits)
-            if "?" in self.visited[cur_room.id].values():
-                # print('found one', cur_room.id)
+        for i in range(5):
+        # while searching:
+            # print('bfs running')
+            # print('cur room', cur_room.id)
+            # print('bfs result', cur_room.id)
+            print(self.visited[cur_room.id])
+            print("?" in self.visited[0].keys())
+            if '?' in self.visited[cur_room.id].values():
+                # go through the existing direction.
+               # find the next place to go
                 searching = False
-                return True 
-
-            if prev_room:
-                print('there is a prev room')
-                # print(f'prev room {prev_room.id} cur room { cur_room.id }')
-                for item in cur_room_exits:
-                    if prev_room.id != self.visited[cur_room.id][item]:
-                        print('checking for right direction')
-                        # print(prev_room.id)
-                        prev_room = cur_room
-                        player.travel[item]
-                        traversalPath.append(item)
-                        cur_room = player.currentRoom
-                        cur_room_exits = cur_room.getExits()
+                return False
             else:
-                player.travel(cur_room_exits[0])
-                traversalPath.append(cur_room_exits[0])
-                cur_room = player.currentRoom
-                cur_room_exits = cur_room.getExits()
-        return False
+                if len(cur_room_exits) <= 1:
+                    print('room before', player.currentRoom.id)
+                    player.travel(cur_room_exits[0])
+                    print('room after', player.currentRoom.id)
+                    prev_room = cur_room
+                    cur_room_exits = cur_room.getExits()
+
+                # for item in cur_room_exits:
+                #     print('direction: ', item)
+                    
+                    # if prev_room.id != self.visited[cur_room.id][item]:
+                    #     prev_room = cur_room
+                    #     player.travel[item]
+                    #     traversalPath.append(item)
+                    #     cur_room = player.currentRoom
+                    #     cur_room_exits = cur_room.getExits()
+            # else:
+            #     player.travel(cur_room_exits[0])
+            #     traversalPath.append(cur_room_exits[0])
+            #     cur_room = player.currentRoom
+            #     cur_room_exits = cur_room.getExits()
+
+
+            #     print('looking for ?')
+            #     # print('checking for visitd room')
+            #     searching = False
+            #     # print(f'searching: {searching} cur room: {cur_room.id}')
+            # else:
+            #     return False
+
+            # if prev_room:
+            #     # print('there is a prev room')
+            #     for item in cur_room_exits:
+            #         if prev_room.id != self.visited[cur_room.id][item]:
+            #             prev_room = cur_room
+            #             player.travel[item]
+            #             traversalPath.append(item)
+            #             cur_room = player.currentRoom
+            #             cur_room_exits = cur_room.getExits()
+            # else:
+            #     player.travel(cur_room_exits[0])
+            #     traversalPath.append(cur_room_exits[0])
+            #     cur_room = player.currentRoom
+            #     cur_room_exits = cur_room.getExits()
+        return True
 
     def depth_first_traverse(self):
         # searching is true
@@ -116,6 +146,8 @@ class Mapping:
         prev_room = None
         # start searching
         while searching:
+            # print('dft running')
+            # print('dft room visit', cur_room.id)
         # for i in range(3):
             # print('visited', self.visited)
             # print(f'cur room {cur_room.id}')
@@ -123,6 +155,9 @@ class Mapping:
                 self.add_room_to_visited(cur_room)
 
             prev_room = cur_room
+            # print('cur room', cur_room.id)
+            # print('cur room exits', self.visited[cur_room.id])
+
             for k, v in self.visited[cur_room.id].items():
                 if v == '?':
                     exit = k
@@ -133,6 +168,7 @@ class Mapping:
             traversalPath.append(exit)
             cur_room = player.currentRoom
             if cur_room.id not in self.visited:
+                # print(f'cur room: {cur_room.id}  exit: {exit}')
                 self.add_room_to_visited(cur_room)
 
             self.visited[prev_room.id][exit] = cur_room.id
@@ -147,12 +183,16 @@ class Mapping:
 m = Mapping(roomGraph)
 # print('graph length', len(roomGraph) )
 # print('visited', len(m.visited) )
-for i in range(13):
-    df = m.depth_first_traverse()
-    print('dead end room', df)
-    print('path length', len(traversalPath))
-    m.breadth_first_search(df)
-    print(m.breadth_first_search(df))
+df = m.depth_first_traverse()
+m.breadth_first_search(df)
+# print('room graph length', len(roomGraph))
+# while len(m.visited) < len(roomGraph):
+# # for i in range(13):
+#     df = m.depth_first_traverse()
+#     # print('dead end room', df)
+#     # print('path length', len(traversalPath))
+#     m.breadth_first_search(df)
+    # print(m.breadth_first_search(df))
 
 
 # TRAVERSAL TEST
