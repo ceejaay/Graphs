73176c5858e14cbeed1dcbe7fcd68e4063bbcda9
Chad Jemmett <chad.jemmett@gmail.com>
2019-03-19 08:22:44 -0600

    Working on the average friends problem

--- projects/graph/social/average.py
+++ projects/graph/social/average.py
@@ -1,4 +1,4 @@
-
+import random
 
 people = 10
 
@@ -8,7 +8,33 @@ for i in range(0, people):
     face_graph[i] = set()
 
 
-# for item in face_graph:
-#     item.add(1)
-print('face_graph', face_graph)
+for item in face_graph:
+    rando_friend = random.randint(0, 10)
+
+
+
+
+
+for item in face_graph:
+   face_graph[item].add(1)
+   total = 0 
+   total += len(face_graph[item])
+   print(total)
+
+
+# print('face_graph', face_graph)
 # print('average', avg)
+
+# loop through each set.
+# check the averate number of friends
+# add a friend connection to a random friend.
+# do that until the average is reached
+
+# plan:
+#     while average is less than the given aveerage.
+#         add a friend connecion at random.
+#         when adding the friend connection...
+#         make sure a friend is not becomign friends with herself.
+
+
+#         check the average
