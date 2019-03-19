8803ef7fb90609a6af580e156304443b351c506e
Chad Jemmett <chad.jemmett@gmail.com>
2019-03-18 20:54:23 -0600

    Scratch paper for average friend problem

---
+++ projects/graph/social/average.py
@@ -0,0 +1,14 @@
+
+
+people = 10
+
+face_graph = {}
+
+for i in range(0, people):
+    face_graph[i] = set()
+
+
+# for item in face_graph:
+#     item.add(1)
+print('face_graph', face_graph)
+# print('average', avg)
